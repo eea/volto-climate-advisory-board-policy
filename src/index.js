a65a5762b4c20edd0c7970566ed618604557aee3
ana-oprea <anaop3212@gmail.com>
2023-03-02 13:04:19 +0200

    feat: add styling to header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import logo from '@eeacms/volto-climate-advisory-board-policy/../theme/assets/images/Header/logo.png';
+
 const applyConfig = (config) => {
+  config.settings.eea.headerOpts = {
+    ...config.settings.eea.headerOpts,
+    logo,
+    // logoWhite,
+  };
   return config;
 };
 
