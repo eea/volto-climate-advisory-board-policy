70cdf312553c78ed4d38cd3617d7d9e3f32ea152
Alexandru Medesan <alexandrumedesan@Alexandru-MacBook-Pro-16.local>
2023-05-03 16:59:51 +0300

    chore: fix lint

--- src/customizations/volto/components/theme/Footer/utils.js
+++ src/customizations/volto/components/theme/Footer/utils.js
@@ -1,8 +1,6 @@
 export const trackPageView = () =>
   window.addEventListener('wtAnalyticsReady', function () {
-    console.log('trackPageView');
     if (window.$wt.analytics.isTrackable()) {
-      console.log('trackPageView - isTrackable');
       window.$wt.trackPageView();
     }
   });
