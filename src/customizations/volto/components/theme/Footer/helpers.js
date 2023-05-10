aa8cd81ccec00b6d46f580d58e26f0283b97e3e3
Alexandru Medesan <alexandrumedesan@Alexandru-MacBook-Pro-16.local>
2023-05-10 11:50:25 +0300

    feat: remove trackPageView from out code since it is already done by load.js - test

--- src/customizations/volto/components/theme/Footer/helpers.js
+++ src/customizations/volto/components/theme/Footer/helpers.js
@@ -1,13 +1,10 @@
 const doEuropaAnalyticsParams = () => {
   const siteID = window.env?.RAZZLE_EUROPA_ANALYTICS_SITE_ID;
-
   const sitePath = [window.env?.RAZZLE_EUROPA_ANALYTICS_SITE_PATH];
-
   const mode = window.env?.RAZZLE_EUROPA_ANALYTICS_MODE;
-
   const instance = window.env?.RAZZLE_EUROPA_ANALYTICS_INSTANCE;
-
   const utility = window.env?.RAZZLE_EUROPA_ANALYTICS_UTILITY;
+
   const result = {
     siteID,
     sitePath,
@@ -18,7 +15,7 @@ const doEuropaAnalyticsParams = () => {
   return result;
 };
 
-const loadEuropaAnalyticsScript = (callback) => {
+const loadEuropaAnalyticsScript = () => {
   const existingScript =
     __CLIENT__ && document.getElementById(`europaAnalyticsJS`);
 
@@ -38,29 +35,7 @@ const loadEuropaAnalyticsScript = (callback) => {
     script2.id = `europaAnalyticsJS`;
     script2.defer = `defer`;
     document.body.appendChild(script2);
-
-    script2.onload = () => {
-      if (callback) callback();
-    };
-  }
-
-  //callback, if needed
-  if (existingScript && callback) {
-    callback();
   }
 };
 
-const startTrackPageView = (callback) => {
-  //check for loaded europaAnalytics script in dom scripts
-  var scripts = __CLIENT__ && document.getElementsByTagName('script');
-  if (scripts) {
-    for (var i = scripts.length; i--; ) {
-      // eslint-disable-next-line eqeqeq
-      if (scripts[i].src === `https://europa.eu/webtools/load.js`) {
-        if (callback) callback();
-      }
-    }
-  } // eslint-disable-next-line eqeqeq
-};
-
-export { loadEuropaAnalyticsScript, startTrackPageView };
+export { loadEuropaAnalyticsScript };
