a5e41ef4c9de3aa5a9cfdc1f66244eeef6d560a7
Cretu Mihaela <cretu.mihaelaa11@gmail.com>
2023-10-09 16:33:15 +0300

    fix cypress

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -39,6 +39,7 @@ describe('Blocks Tests', () => {
 
     // Configure block
     cy.getSlate().click().type('Hero Block');
+    cy.get('#sidebar .formtabs').contains('Block').click();
     cy.get('.ui.checkbox').contains('Full height').click();
     cy.get('.ui.checkbox').contains('Hide publishing date').click();
 
