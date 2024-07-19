ff7c1e226bb0d33284bfc85158878c76214ad3aa
Teodor <teodorvoicu87@gmail.com>
2024-07-19 14:20:38 +0300

    revert to previous test file

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -8,26 +8,46 @@ describe('Blocks Tests', () => {
     // Change page title
     cy.clearSlateTitle();
     cy.getSlateTitle().type('My Add-on Page');
-
     cy.get('.documentFirstHeading').contains('My Add-on Page');
-
-    cy.getSlate().click({ force: true });
+    cy.getSlate().click();
 
     // Add block
-    cy.get('.ui.basic.icon.button.block-add-button')
-      .first()
-      .click({ force: true });
-    cy.get('.blocks-chooser .title').contains('Media').click({ force: true });
-    cy.get('.content.active.media .button.image')
-      .contains('Image')
-      .click({ force: true });
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Media').click();
+    cy.get('.content.active.media .button.image').contains('Image').click();
 
     // Save
-    cy.get('#toolbar-save').click({ force: true });
+    cy.get('#toolbar-save').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
 
     // then the page view should contain our changes
     cy.contains('My Add-on Page');
     cy.get('.block.image');
+  });
+
+  it('Add Hero Block', () => {
+    // Change page title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('My Add-on Page');
+    cy.get('.documentFirstHeading').contains('My Add-on Page');
+    cy.getSlate().click();
+
+    // Add block
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active.common .button.hero')
+      .contains('Hero')
+      .click({ force: true });
+
+    // Configure block
+    cy.getSlate().click().type('Hero Block');
+
+    // Save
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    // then the page view should contain our changes
+    cy.contains('My Add-on Page');
+    cy.get('.hero-block');
   });
 });
